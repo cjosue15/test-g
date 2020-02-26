86edab4334cde0b2be66991738a949962dc879eb
Carlos Josue <carlosjosuemn98@gmail.com>
2020-02-26 15:42:34 -0500

    Test G Logic Ok

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ import Step4 from './components/Test4/Step4';
 
 import './App.css';
 import Timer from './components/Timer';
-// import ErrorsForm from './components/ErrorsForm';
 
 class App extends React.Component {
   constructor(props) {
@@ -25,178 +24,51 @@ class App extends React.Component {
       test1: {},
       test2: {},
       test3: {},
-      test4: {},
-      errorTest1: {
-        1: false,
-        2: false,
-        3: false,
-        4: false,
-        5: false,
-        6: false,
-        7: false,
-        8: false,
-        9: false,
-        10: false,
-        11: false,
-        12: false
-      },
-      errorTest2: {
-        1: false,
-        2: false,
-        3: false,
-        4: false,
-        5: false,
-        6: false,
-        7: false,
-        8: false,
-        9: false,
-        10: false,
-        11: false,
-        12: false,
-        13: false,
-        14: false
-      },
-      errorTest3: {
-        1: false,
-        2: false,
-        3: false,
-        4: false,
-        5: false,
-        6: false,
-        7: false,
-        8: false,
-        9: false,
-        10: false,
-        11: false,
-        12: false
-      },
-      errorTest4: {
-        1: false,
-        2: false,
-        3: false,
-        4: false,
-        5: false,
-        6: false,
-        7: false,
-        8: false
-      },
-      formValid: false,
-      showErrors: false
+      test4: {}
     };
   }
 
   handleChange1 = event => {
-    const { name, value, checked } = event.target;
-    this.setState(
-      {
-        test1: {
-          ...this.state.test1,
-          [name]: value
-        },
-        errorTest1: {
-          ...this.state.errorTest1,
-          [name]: checked
-        }
-      },
-      this.validateForm
-    );
+    const { name, value } = event.target;
+    this.setState({
+      test1: {
+        ...this.state.test1,
+        [name]: value
+      }
+    });
   };
 
   handleChange2 = event => {
-    const { name, value, checked } = event.target;
-    this.setState(
-      {
-        test2: {
-          ...this.state.test2,
-          [name]: value
-        },
-        errorTest2: {
-          ...this.state.errorTest2,
-          [name]: checked
-        }
-      },
-      this.validateForm
-    );
+    const { name, value } = event.target;
+    this.setState({
+      test2: {
+        ...this.state.test2,
+        [name]: value
+      }
+    });
   };
 
   handleChange3 = event => {
-    const { name, value, checked } = event.target;
-    this.setState(
-      {
-        test3: {
-          ...this.state.test3,
-          [name]: value
-        },
-        errorTest3: {
-          ...this.state.errorTest3,
-          [name]: checked
-        }
-      },
-      this.validateForm
-    );
+    const { name, value } = event.target;
+    this.setState({
+      test3: {
+        ...this.state.test3,
+        [name]: value
+      }
+    });
   };
 
   handleChange4 = event => {
-    const { name, value, checked } = event.target;
-    this.setState(
-      {
-        test4: {
-          ...this.state.test4,
-          [name]: value
-        },
-        errorTest4: {
-          ...this.state.errorTest4,
-          [name]: checked
-        }
-      },
-      this.validateForm
-    );
-  };
-
-  validateForm = () => {
-    let isValid = false;
-    const radioStates1 = this.state.errorTest1;
-    const radioStates2 = this.state.errorTest2;
-    const radioStates3 = this.state.errorTest3;
-    const radioStates4 = this.state.errorTest4;
-
-    const checkedStatus1 = Object.keys(radioStates1).map(key => {
-      return radioStates1[key];
+    const { name, value } = event.target;
+    this.setState({
+      test4: {
+        ...this.state.test4,
+        [name]: value
+      }
     });
-
-    const checkedStatus2 = Object.keys(radioStates2).map(key => {
-      return radioStates2[key];
-    });
-
-    const checkedStatus3 = Object.keys(radioStates3).map(key => {
-      return radioStates3[key];
-    });
-
-    const checkedStatus4 = Object.keys(radioStates4).map(key => {
-      return radioStates4[key];
-    });
-
-    const checkedStatus = [
-      ...checkedStatus1,
-      ...checkedStatus2,
-      ...checkedStatus3,
-      ...checkedStatus4
-    ];
-
-    const filteredArray = checkedStatus.filter(value => value === false);
-
-    if (filteredArray.length === 0) {
-      isValid = true;
-    }
-
-    this.setState({ formValid: isValid });
   };
 
   handleSubmit = event => {
-    // event.preventDefault();
-    // if (!this.state.formValid) {
-    //   return;
-    // }
     const { test1 } = this.state;
     const { test2 } = this.state;
     const { test3 } = this.state;
@@ -270,41 +142,6 @@ class App extends React.Component {
     }
     return null;
   }
-
-  sendButton() {
-    const currentStep = this.state.currentStep;
-    if (currentStep === 20)
-      return (
-        <button
-          className='btn btn-primary'
-          onClick={() => {
-            this.validateForm();
-            this.showError();
-          }}
-        >
-          Terminar Test
-        </button>
-      );
-  }
-
-  showError() {
-    this.setState({ showErrors: true });
-  }
-
-  // showDivError() {
-  //   if (this.state.showErrors) {
-  //     if (!this.state.formValid) {
-  //       return (
-  //         <div className='ErrorsForm'>
-  //           <ErrorsForm test={'Test 1'} errors={this.state.errorTest1} />
-  //           <ErrorsForm test={'Test 2'} errors={this.state.errorTest2} />
-  //           <ErrorsForm test={'Test 3'} errors={this.state.errorTest3} />
-  //           <ErrorsForm test={'Test 4'} errors={this.state.errorTest4} />
-  //         </div>
-  //       );
-  //     }
-  //   }
-  // }
 
   callBackTimer = (seconds, minutes, step) => {
     if (seconds === 0 && minutes === 0) {
@@ -456,18 +293,6 @@ class App extends React.Component {
       <React.Fragment>
         <Navbar />
         <div className='container'>
-          {/* <p>Step {this.state.currentStep} </p> */}
-          {/* {this.state.showErrors && !this.state.formValid && (
-            <div className='ErrorsForm'>
-              <ErrorsForm test={'Test 1'} errors={this.state.errorTest1} />
-              <ErrorsForm test={'Test 2'} errors={this.state.errorTest2} />
-              <ErrorsForm test={'Test 3'} errors={this.state.errorTest3} />
-              <ErrorsForm test={'Test 4'} errors={this.state.errorTest4} />
-            </div>
-          )} */}
-
-          {/* {this.showDivError()} */}
-
           <form onSubmit={this.handleSubmit}>
             {this.Step1()}
             {this.Step2()}
@@ -478,7 +303,6 @@ class App extends React.Component {
               <div className='conten-buttons'>
                 {this.previousButton()}
                 {this.nextButton()}
-                {this.sendButton()}
               </div>
             )}
           </form>
